3762f6f8e3ce5a8094e0c0d66baa7daacc1cbeb3
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2024-04-29 16:44:11 +0200

    randomise generation for workers

--- index.php
+++ index.php
@@ -11,11 +11,19 @@
     echo "Hello World!";
     for($i=0;$i<10;++$i){
         $faker = Faker\Factory::create("pl_PL");
-        echo "<div>";
-            echo ($i + 1).".&nbsp;";
-            echo "<p class='firstName'>".$faker->firstName()."</p>";
-            echo "<p class='lastName'>".$faker->lastName()."</p>";
-            echo "<p class='email'>".$faker->email()."</p>";
+        echo "<div style='display:flex;'>";
+            echo "<p>".($i + 1),","."</p>";
+            echo "<p class='firstName'>".$faker->firstName().","."</p>";
+            echo "<p class='lastName'>".$faker->lastName().","."</p>";
+            echo "<p class='numberRole'>"."1".","."</p>";
+            echo "<p class='number'>"."+48".(rand(1,9));
+            for($j = 0; $j<8; ++$j){
+                echo rand(0,9);
+            }
+            echo ",</p>";
+            echo "<p class='oddział'>".$faker->address().","."</p>"; // 7 oddziałów random
+            echo "<p class='biuro'>".$faker->country().","."</p>"; // 7 oddziałów random
+            echo "<p class='zatrudniony'>".(rand()%2)."</p>"; // 7 oddziałów random
         echo "</div>";
     }
     ?>
