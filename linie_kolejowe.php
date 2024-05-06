4ce61f876725b672daf166e4458f09812ce59fa1
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2024-05-06 19:14:40 +0200

    Railway lines + made the code more readable

--- linie_kolejowe.php
+++ linie_kolejowe.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test PHP</title>
+    <title>LK PHP</title>
 </head>
 <body>
     <?php
@@ -22,37 +22,19 @@
                 $city2 = $faker->city();
             }
             array_push($cities, $city2);
-            $str = ($i+1).",".$city.",".$city2.",".$faker->numberBetween(25, 354);
-            echo $str; // 7 oddziałów random
-            /*echo "<p class='lat_y'>".$faker->latitude($min = 49.50, $max = 54) .","."</p>";
-            echo "<p class='long_x'>".$faker->longitude($min = 14.50, $max = 23.5).","."</p>"; // 7 oddziałów random*/
-            /*echo "<p class='zatrudniony'>".(rand()%2)."</p>"; // 7 oddziałów random*/
+            $str = ($i+1).","; // numer linii kolejowej
+            $str .= $city.","; //stacja początkowa
+            $str .= $city2.","; // stacja końcowa
+            $str .= $faker->numberBetween(25, 354); //dystans
+            echo $str; 
         echo "</div>";
     }
     echo "all cities";
     echo "<div style='display:flex;'>";
     for($i=0;$i<50;++$i){
-        echo '"'.$cities[$i].'"'.",";
+        echo '"'.$cities[$i].'"'.","; // lista miast które już są jako początek / koniec LK i nie mogą występować pomiędzy
     }
     echo "</div>";
-    /*
-    echo "new line";
-    //cities not in a LK
-    for($i=0;$i<125;++$i){
-    $faker = Faker\Factory::create("pl_PL");
-    echo "<div style='display:flex;'>";
-    $city = $faker->city();
-    while(in_array($city, $cities)){
-        $city = $faker->city();
-    }
-    array_push($cities, $city);
-    $str = ($i+1).",".$city;
-    echo $str; // 7 oddziałów random
-    /*echo "<p class='lat_y'>".$faker->latitude($min = 49.50, $max = 54) .","."</p>";
-    echo "<p class='long_x'>".$faker->longitude($min = 14.50, $max = 23.5).","."</p>"; // 7 oddziałów random*/
-    /*echo "<p class='zatrudniony'>".(rand()%2)."</p>"; // 7 oddziałów random*/
-    /*echo "</div>";*/
-    /*}*/
     ?>
 </body>
 </html>
