864bb1d827306e48870cab7e83e0f9d30a27ec3a
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2024-05-06 13:36:26 +0200

    city generation

--- index.php
+++ index.php
@@ -9,12 +9,22 @@
     <?php
     require_once 'vendor/autoload.php';
     echo "Hello World!";
+    $cities = [];
     for($i=0;$i<10;++$i){
         $faker = Faker\Factory::create("pl_PL");
         echo "<div style='display:flex;'>";
             echo "<p>".($i + 1),","."</p>";
-            echo "<p class='firstName'>".$faker->firstName().","."</p>";
-            echo "<p class='lastName'>".$faker->lastName().","."</p>";
+            $name = $faker->name();
+            $name = explode(" ", $name);
+            /*for($j=0;$j<count($name);++$j){
+                echo $name[$j];
+            }*/
+            $counter = 0;
+            while(str_ends_with($name[$counter], ".") || $name[$counter] == "dr" || $name[$counter] == "mgr"){
+                $counter++;
+            }
+            echo "<p class='firstName'>".$name[$counter].","."</p>";
+            echo "<p class='lastName'>".$name[$counter+1].","."</p>";
             echo "<p class='numberRole'>"."1".","."</p>";
             echo "<p class='number'>"."+48".(rand(1,9));
             for($j = 0; $j<8; ++$j){
@@ -23,6 +33,14 @@
             echo ",</p>";
             echo "<p class='oddział'>".$faker->address().","."</p>"; // 7 oddziałów random
             echo "<p class='biuro'>".$faker->country().","."</p>"; // 7 oddziałów random
+            $city = $faker->city();
+            while(in_array($city, $cities)){
+                $city = $faker->city();
+            }
+            array_push($cities, $city);
+            echo "<p class='miasto'>".end($cities).","."</p>"; // 7 oddziałów random
+            echo "<p class='lat_y'>".$faker->latitude($min = 49.50, $max = 54) .","."</p>";
+            echo "<p class='long_x'>".$faker->longitude($min = 14.50, $max = 23.5).","."</p>"; // 7 oddziałów random
             echo "<p class='zatrudniony'>".(rand()%2)."</p>"; // 7 oddziałów random
         echo "</div>";
     }
