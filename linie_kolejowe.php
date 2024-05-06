5f88248e0d456abf3941c56e80c2fc5cc0f55d9a
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2024-05-06 14:11:11 +0200

    miasta + linie_kolejowe

--- linie_kolejowe.php
+++ linie_kolejowe.php
@@ -22,7 +22,24 @@
                 $city2 = $faker->city();
             }
             array_push($cities, $city2);
-            $str = ($i+1).",".$city.",".$city2.",".$faker->numberBetween(25, 354);;
+            $str = ($i+1).",".$city.",".$city2.",".$faker->numberBetween(25, 354);
+            echo $str; // 7 oddziałów random
+            /*echo "<p class='lat_y'>".$faker->latitude($min = 49.50, $max = 54) .","."</p>";
+            echo "<p class='long_x'>".$faker->longitude($min = 14.50, $max = 23.5).","."</p>"; // 7 oddziałów random*/
+            /*echo "<p class='zatrudniony'>".(rand()%2)."</p>"; // 7 oddziałów random*/
+        echo "</div>";
+    }
+    echo "new line";
+    //cities not in a LK
+    for($i=0;$i<125;++$i){
+        $faker = Faker\Factory::create("pl_PL");
+        echo "<div style='display:flex;'>";
+            $city = $faker->city();
+            while(in_array($city, $cities)){
+                $city = $faker->city();
+            }
+            array_push($cities, $city);
+            $str = ($i+1).",".$city;
             echo $str; // 7 oddziałów random
             /*echo "<p class='lat_y'>".$faker->latitude($min = 49.50, $max = 54) .","."</p>";
             echo "<p class='long_x'>".$faker->longitude($min = 14.50, $max = 23.5).","."</p>"; // 7 oddziałów random*/
